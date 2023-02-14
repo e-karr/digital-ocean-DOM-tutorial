5ab4e1b4ba9903414105ac7efbf32b06cf96476b
Elizabeth Karr <e.karr93@icloud.com>
2023-02-14 16:21:34 -0600

    event listeners

--- js/events.js
+++ js/events.js
@@ -1,10 +1,18 @@
 // Function to modify the text content of the paragraph
 const changeText = () => {
-	const p = document.querySelector('p');
-
 	p.textContent = "I changed because of an inline event handler.";
 }
 
+const alertText = () => {
+	alert('Will I alert?');
+}
+
 // Add event handler as a property of the button element
+// const button = document.querySelector('button');
+// button.onclick = changeText;
+
+// Listen for click event
 const button = document.querySelector('button');
-button.onclick = changeText;
+const p = document.querySelector('p');
+button.addEventListener('click', changeText);
+button.addEventListener('click', alertText);
